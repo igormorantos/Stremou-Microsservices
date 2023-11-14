d9711312436b6c3c29e1b9e4ce2d6ca24dfb93de
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-14 16:42:03 -0300

    feat: building repository contracts

---
+++ src/Modules/Stremou.Domain/Contracts/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Stremou.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stremou.Domain.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmail(string email);
+
+        Task<IEnumerable<User>> GetList();
+
+        Task Add(User user);
+
+        void Update(User user);
+
+        void Remove(User user);
+    }
+}
---
+++ src/Modules/Stremou.Domain/Contracts/IVideoRepository.cs
@@ -0,0 +1,24 @@
+﻿using Stremou.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stremou.Domain.Contracts
+{
+    public interface IVideoRepository
+    {
+        Task<Video> GetByVideoUrl(string videoUrl);
+
+        Task<Video> GetByName(string name);
+
+        Task<IEnumerable<Video>> GetAllAsync();
+
+        Task AddAsync(Video video);
+
+        Task UpdateAsync(Video video);
+
+        Task DeleteAsync(Video video);
+    }
+}
