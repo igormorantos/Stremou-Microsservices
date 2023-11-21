1022e2858a411a4c5452108ffdf854f56d2cf6d6
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-21 17:44:53 -0300

    building service implementation

---
+++ src/Modules/Stremou.Modules.Application/Services/Implementations/UserAppService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Stremou.Domain.Contracts;
+using Stremou.Domain.Models;
+using Stremou.Modules.Application.Handlers.User.Commands;
+using Stremou.Modules.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stremou.Modules.Application.Services.Imp
+{
+    public class UserAppService : IUserAppService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public UserAppService(IUserRepository userRepository, IMapper mapper, ILogger logger)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _logger = logger;
+
+        }
+
+        public async Task<UserViewModel> GetByCPF(Cpf cpf)
+        {
+            return _mapper.Map<UserViewModel>(await _userRepository.GetByCpf(cpf));
+        }
+
+        public async Task<UserViewModel> GetByEmail(string email)
+        {
+            return _mapper.Map<UserViewModel>(await _userRepository.GetByEmail(email));
+
+        }
+
+        public async Task Save(CreateUserCommand commandCreate)
+        {
+            await _userRepository.Add(commandCreate.GetEntity());
+        }
+    }
+}
---
+++ src/Modules/Stremou.Modules.Application/Services/Implementations/VideoAppService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Stremou.Domain.Contracts;
+using Stremou.Domain.Models;
+using Stremou.Modules.Application.Handlers.Video.Commands;
+
+namespace Stremou.Modules.Application.Services.Implementations
+{
+    public class VideoAppService : IVideoAppService
+    {
+        private readonly IVideoRepository _videoRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public VideoAppService(IVideoRepository videoRepository, IMapper mapper, ILogger logger)
+        {
+            _videoRepository = videoRepository;
+            _mapper = mapper;
+            _logger = logger;
+
+        }
+        public async Task<VideoViewModel> GetByName(string name)
+        {
+            return _mapper.Map<VideoViewModel>(await _videoRepository.GetByName(name));
+        }
+
+        public async Task<VideoViewModel> GetbyVideoUrl(string url)
+        {
+            return _mapper.Map<VideoViewModel>(await _videoRepository.GetByVideoUrl(url));
+
+        }
+
+        public async Task Save(CreateVideoCommand commandCreate)
+        {
+            await _videoRepository.Add(commandCreate.GetEntity());
+        }
+    }
+}
